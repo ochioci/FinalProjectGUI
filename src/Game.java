99df071fe2018d5cb53a0ca3b6730cc7095e5250
BT_1E10_20 <sample@bths.edu>
2022-05-17 12:27:46 -0400

    first functional build!

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,7 @@
 import java.time.Instant;
 public class Game {
     private int score = 0;
-    private double timeLimit = 100.0;
+    private double timeLimit = 15.0;
     private double startTime = -1.0;
     private Board gameBoard;
     public Game () {
@@ -12,17 +12,33 @@ public class Game {
         score = 0;
         startTime = Instant.now().getEpochSecond();
     }
+
+    public void displayBoard() {
+        gameBoard.printBoard();
+    }
+
     public int playWord(String word) {
         if (Instant.now().getEpochSecond() > startTime + timeLimit) {
             return -1; //out of time
+        }
+        if (!gameBoard.validateWord(word)) {
+            return -2; // not found on board
         }
         if (Dictionary.checkWord(word)) {
             score += getPointValue(word);
             return 1; //success
         } else {
-            return 0; //invalid word;
+            return 0; //invalid word - not in dictionary;
         }
 
+    }
+
+    public void displayTimeRemaining() {
+        System.out.println("You have " + (startTime + timeLimit - Instant.now().getEpochSecond()) + " time remaining.");
+    }
+
+    public void displayScore () {
+        System.out.println("You scored " + score + " points!");
     }
 
     public void playLastBoard() {
--- src/Runner.java
+++ src/Runner.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 public class Runner {
-    private static Game myGame;
+    private static Game myGame = new Game();
     public static void main (String[] args) {
-        Game myGame = new Game();
-        Scanner s = new Scanner(System.in);
         menu();
+    }
+
+
+    public static void dash() {
+        System.out.println(" ------------------------ ");
     }
 
     public static void menu() {
@@ -14,11 +17,59 @@ public class Runner {
         System.out.println("Replay the [s]ame board?");
         System.out.println("[q]uit?");
         String choice = choiceScanner.nextLine();
+        Scanner wordInputScanner = new Scanner(System.in);
         if (choice.equals("n")) {
             myGame.playNewBoard();
-
+            boolean playing = true;
+            while (playing) {
+                myGame.displayBoard();
+                System.out.println("Input a word > ");
+                String inWord = wordInputScanner.nextLine();
+                int result = myGame.playWord(inWord);
+                if (result == -1) {
+                    dash();
+                    System.out.println("Sorry! Time's up");
+                    playing = false;
+                } else if (result == 0) {
+                    System.out.println("Sorry! Invalid word");
+                    myGame.displayTimeRemaining();
+                } else if (result == 1) {
+                    System.out.println("Nice!");
+                    myGame.displayTimeRemaining();
+                } else if (result == -2) {
+                    System.out.println("Sorry! That word was not found on the board");
+                    myGame.displayTimeRemaining();
+                }
+            }
+            myGame.displayScore();
+            dash();
+            menu();
         } else if (choice.equals("s")) {
             myGame.playLastBoard();
+            boolean playing = true;
+            while (playing) {
+                myGame.displayBoard();
+                System.out.println("Input a word > ");
+                String inWord = wordInputScanner.nextLine();
+                int result = myGame.playWord(inWord);
+                if (result == -1) {
+                    dash();
+                    System.out.println("Sorry! Time's up");
+                    playing = false;
+                } else if (result == 0) {
+                    System.out.println("Sorry! Invalid word");
+                    myGame.displayTimeRemaining();
+                } else if (result == 1) {
+                    System.out.println("Nice!");
+                    myGame.displayTimeRemaining();
+                } else if (result == -2) {
+                    System.out.println("Sorry! That word was not found on the board");
+                    myGame.displayTimeRemaining();
+                }
+            }
+            myGame.displayScore();
+            dash();
+            menu();
         } else if (choice.equals("q")) {
             return;
         }
