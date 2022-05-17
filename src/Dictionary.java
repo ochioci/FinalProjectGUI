57da4e068d00b50be597fcbd5161f7a0287f8172
BT_1E10_20 <sample@bths.edu>
2022-05-17 12:30:03 -0400

    note dataset

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,5 +1,6 @@
 public class Dictionary {
     public static boolean checkWord (String word) {
         return true;
+        //tk: use this https://github.com/dwyl/english-words/blob/master/words.txt
     }
 }
